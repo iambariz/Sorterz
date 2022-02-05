2b84b0a5cff66bc147ec5beb27dd0c2f7c262f4d
David Barkanyi <61651210+iambariz@users.noreply.github.com>
2022-02-05 09:55:20 +0000

    Start

---
+++ app.js
@@ -0,0 +1,18 @@
+const buttons = document.querySelectorAll(".project-btn");
+
+const items = document.querySelectorAll(".sorterz-item");
+
+buttons.forEach(function (button) {
+	button.addEventListener("click", function () {
+		dissAppear(this.dataset.id);
+	});
+});
+
+function dissAppear(stays) {
+	items.forEach(function (item) {
+		console.log(item);
+		if (!item.classList.contains(stays)) {
+			item.style.display = "none";
+		}
+	});
+}
